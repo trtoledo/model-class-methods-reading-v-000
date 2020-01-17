5681271d8c5279de501f3913d846dcaea60d1bf6
Tomas Toledo <trtoledo1977@gmail.com>
2020-01-17 23:18:08 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   # filter the @posts list based on user input
   if !params[:author].blank?
-    @posts = Post.where(author: params[:author])
+    @posts = Post.by_author(params[:author])
   elsif !params[:date].blank?
     if params[:date] == "Today"
       @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
